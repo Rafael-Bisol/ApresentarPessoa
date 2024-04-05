a34446a1d955a371788f588a456ac6edfc7ba19a
Rafael Bisol <rafaelpbisol@hotmail.com>
2024-04-05 19:41:51 -0300

    Corrigido erro no cabeçalho de Main

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 class Program
 {
-    public void Main(string[] args)
+    static public void Main(string[] args)
     {
         Console.WriteLine("Bem-vindo!");
         Pessoa pessoa = new Pessoa("Velho Chato", 1000, 1.75);
